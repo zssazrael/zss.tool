ea2429b1b858802e443a0ad2e1f39d27e9a7219d
ZssAzrael <zsszbox@qq.com>
2017-09-03 19:06:00 +0800

    commit

---
+++ src/main/java/zss/tool/Result.java
@@ -0,0 +1,37 @@
+package zss.tool;
+
+@Version("2017.09.03")
+public class Result extends StringObjectMap {
+    private static final long serialVersionUID = 20170903112936L;
+    public static final String RESULT = "result";
+    public static final String MESSAGE = "message";
+
+    public static Result newResult(final boolean result, final String message) {
+        final Result value = new Result();
+        value.setResult(result);
+        value.setMessage(message);
+        return value;
+    }
+
+    public static Result newResult(final boolean result) {
+        final Result value = new Result();
+        value.setResult(result);
+        return value;
+    }
+
+    public String getMessage() {
+        return getString(MESSAGE, "");
+    }
+
+    public void setMessage(final String message) {
+        setString(MESSAGE, message);
+    }
+
+    public boolean getResult() {
+        return getBoolean(RESULT, false);
+    }
+
+    public void setResult(final boolean result) {
+        setBoolean(RESULT, result);
+    }
+}
