060604df91bc771039c7d5ca86fa496ff5becce7
ZssAzrael <zsszbox@qq.com>
2018-01-10 15:09:33 +0800

    commit

--- src/main/java/zss/tool/NumberTool.java
+++ src/main/java/zss/tool/NumberTool.java
@@ -1,6 +1,8 @@
 package zss.tool;
 
-@Version("2017.12.19")
+import java.math.BigDecimal;
+
+@Version("2018.01.10")
 public class NumberTool {
     public static Integer defaultInteger(final Integer value, final Integer defaultValue) {
         if (value == null) {
@@ -14,5 +16,16 @@ public class NumberTool {
             return Integer.valueOf(0);
         }
         return value;
+    }
+
+    public static BigDecimal defaultBigDecimal(final BigDecimal value, final BigDecimal defaultValue) {
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public static BigDecimal defaultBigDecimal(final BigDecimal value) {
+        return defaultBigDecimal(value, BigDecimal.ZERO);
     }
 }
