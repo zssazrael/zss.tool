aff04b066b119293bde8d068a18bc13dd69b271d
ZssAzrael <zsszbox@qq.com>
2018-03-26 11:36:33 +0800

    commit

---
+++ src/main/java/zss/tool/NumberTool.java
@@ -0,0 +1,55 @@
+package zss.tool;
+
+import java.math.BigDecimal;
+
+@Version("2018.02.09")
+public class NumberTool {
+    public static Integer defaultInteger(final Integer value, final Integer defaultValue) {
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public static Integer defaultInteger(final Integer value) {
+        if (value == null) {
+            return Integer.valueOf(0);
+        }
+        return value;
+    }
+
+    public static BigDecimal defaultBigDecimal(final BigDecimal value, final BigDecimal defaultValue) {
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public static BigDecimal defaultBigDecimal(final BigDecimal value) {
+        return defaultBigDecimal(value, BigDecimal.ZERO);
+    }
+
+    public static BigDecimal max(BigDecimal value1, BigDecimal value2) {
+        value1 = defaultBigDecimal(value1);
+        value2 = defaultBigDecimal(value2);
+        return value1.compareTo(value2) > 0 ? value1 : value2;
+    }
+
+    public static BigDecimal min(BigDecimal value1, BigDecimal value2) {
+        value1 = defaultBigDecimal(value1);
+        value2 = defaultBigDecimal(value2);
+        return value1.compareTo(value2) < 0 ? value1 : value2;
+    }
+
+    public static boolean greaterThan(BigDecimal value1, BigDecimal value2) {
+        value1 = defaultBigDecimal(value1);
+        value2 = defaultBigDecimal(value2);
+        return value1.compareTo(value2) > 0;
+    }
+
+    public static boolean lessThan(BigDecimal value1, BigDecimal value2) {
+        value1 = defaultBigDecimal(value1);
+        value2 = defaultBigDecimal(value2);
+        return value1.compareTo(value2) < 0;
+    }
+}
